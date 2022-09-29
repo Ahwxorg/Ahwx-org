3f91586fe73854dfa52c195559dd1e1b3de73376
root <root@proxbox.kpn>
2022-09-29 04:21:03 +0200

    Update some things including adding a Git server as mirror from GitHub.

--- projects/script.js
+++ projects/script.js
@@ -1,15 +1,34 @@
-var xmlhttp = new XMLHttpRequest();
-var url = "https://api.github.com/users/Ahwxx/repos";
-xmlhttp.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    var arr = JSON.parse(this.responseText);
-    var out = "";
-    var i;
-    for (i = 0; i < arr.length; i++) {
-      out += '<div class="project"><div class="pink"><a href="' + arr[i].html_url + '">' + arr[i].name + '</a></div> ' + arr[i].description + '<br> ' + arr[i].language + '</div> <br>';
-    }
-    document.getElementById("projects").innerHTML += out;
-  }
-};
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
+const projects = document.getElementById("projects");
+
+                    var xmlhttp = new XMLHttpRequest();
+                    var url = "https://api.github.com/users/Ahwxorg/repos";
+                    xmlhttp.onreadystatechange = function() {
+                        if (this.readyState == 4 && this.status == 200) {
+                            var arr = JSON.parse(this.responseText);
+                            var out = "";
+                            var i;
+                            for (i = 0; i < arr.length; i++) {
+                                if (i <= 100) {
+                                    out += `
+                                        <div class="project">
+                                            <div class="big">` + arr[i].name + `</div>`;
+                                    if (arr[i].description != null) {
+                                        out += `<p>` + arr[i].description + `</p>`;
+                                    }
+                                    if (arr[i].language != null) {
+                                        out += `<div class="ghs">
+                                                    <div>` + arr[i].language + `</div>
+                                                </div>`;
+                                    }
+                                    out += `<div class="ghs">
+                                                <a href="` + arr[i].html_url + `">Repository</a>
+                                            </div>
+                                        </div>`;
+                                }
+                            }
+                            projects.innerHTML = out;
+                        }
+                    };
+                    xmlhttp.open("GET", url, true);
+                    xmlhttp.send();
+
