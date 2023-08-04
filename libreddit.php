058281219d8612d175d9978ae1bbf25e48b589cc
Ahwx <ahwx@ahwx.org>
2023-08-04 22:42:54 +0200

    feat: more pages n shit

---
+++ libreddit.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ahwx >> Libreddit</title>
+    <link rel="shortcut icon" href="img/avatar.png">
+    <meta name="description" content="Ahwx >> Libreddit">
+    <meta property="og:title" content="Ahwx >> Libreddit">
+    <meta property="og:site_name" content="ahwx.org">
+    <meta property="og:url" content="https://ahwx.org">
+    <meta property="og:description" content="Ahwx >> Libreddit">
+    <meta property="og:type" content="website">
+    <meta name="twitter:title" content="Ahwx >> Libreddit">
+    <meta name="twitter:description" content="Ahwx >> Libreddit">
+    <meta name="keywords" content="ahwx, linux, gnu, gentoo, void, arch, foss, floss, oss, html, css, blog, computing, computers, nas, personal website, open source, tutorials, tutorial, libre, git, arch linux, archlinux, openbsd, freebsd, bsd, macos, macbook, thinkpad, lenovo thinkpad, lenovo, ibm, ibm thinkpad, networking">
+    <link href="../img/avatar.png" type="image/png" rel="icon">
+    <link href="../css/style.css" type="text/css" rel="stylesheet">
+    <link rel="canonical" href="https://ahwx.org"/>
+  </head>
+	<body>
+		<div class="navbar-holder">
+			<header class="navbar">
+				<div class="avatar"><img class="mono-image" src="/img/avatar.png" alt="avatar"/></div>
+				<h1><a class="headertext" href="/">ahwx</a></h1>
+				<nav class="topnav">
+					<ul>
+						<li><a href="https://blog.ahwx.org">blog</a></li>
+						<li><a href="https://git.ahwx.org">git</a></li>
+						<li><a href="/contact.php">contact</a></li>
+						<li><a href="/webrings.php">webrings</a></li>
+						<li><a href="/instances.php">instances</a></li>
+						<li><a href="/other/">other</a></li>
+            <br>
+					</ul>
+				</nav>
+			</header>
+		</div>
+    <main>
+      <div class="body">
+        <br>
+        <h1>Libreddit</h1>
+	<p>
+          Hello there! Thank you for visiting my Libreddit instance. It's currently down due to issues with the Reddit API.
+        </p>
+
+         </br><br>
+        </p>
+      </div>
+      <br>
+      <div class="body">
+        <p><br>
+          Join my Matrix groupchat (#main:ahwx.org)</a>!<br><br>
+          If you found something wrong, please send me an <a href="mailto:ahwx@ahwx.org">email</a> or a message on <a href="https://matrix.to/#/@ahwx:ahwx.org">Matrix</a> (@ahwx:ahwx.org)
+        </p><br>
+      </div>
+    </main>
+	</body>
+</html>
